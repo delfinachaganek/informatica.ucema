078ba2956b30d8e8c076fce6a76a0d79777f43b4
delfina <delchaga@hotmail.com>
2021-04-20 10:01:07 -0300

    ejercicio10tp3

--- tp3/ejercicio 10 tp3.py
+++ tp3/ejercicio 10 tp3.py
@@ -1,3 +1,7 @@
 #Obtené las substrings y las posiciones de estas en una string dada considerando que las substrings 
 #están delimitadas por los caracteres @ o &.
 #ejercicio 10
+import re
+texto = " Realiza @ unprograma que& encuentre una palabra unidaa otra conunguionbajo"
+palabra_a_buscar = re.findall("@(.*)&",texto)
+print(palabra_a_buscar)
